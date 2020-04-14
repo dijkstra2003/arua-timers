cd8f2e2d40c6f43eb0b39bf2127f806aca8ef81f
Luc Dijsktra <dijkstra2003@gmail.com>
2020-04-14 14:51:43 +0200

    added monsters and maps, also in db

---
+++ api/src/Entity/Map.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\MapRepository")
+ */
+class Map
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $planet;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Monster", mappedBy="map")
+     */
+    private $monsters;
+
+    public function __construct()
+    {
+        $this->monsters = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlanet(): ?string
+    {
+        return $this->planet;
+    }
+
+    public function setPlanet(string $planet): self
+    {
+        $this->planet = $planet;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Monster[]
+     */
+    public function getMonsters(): Collection
+    {
+        return $this->monsters;
+    }
+
+    public function addMonster(Monster $monster): self
+    {
+        if (!$this->monsters->contains($monster)) {
+            $this->monsters[] = $monster;
+            $monster->setMap($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMonster(Monster $monster): self
+    {
+        if ($this->monsters->contains($monster)) {
+            $this->monsters->removeElement($monster);
+            // set the owning side to null (unless already changed)
+            if ($monster->getMap() === $this) {
+                $monster->setMap(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Monster.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\MonsterRepository")
+ */
+class Monster
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $respawn_time;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Map", inversedBy="monsters")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $map;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRespawnTime(): ?\DateTimeInterface
+    {
+        return $this->respawn_time;
+    }
+
+    public function setRespawnTime(\DateTimeInterface $respawn_time): self
+    {
+        $this->respawn_time = $respawn_time;
+
+        return $this;
+    }
+
+    public function getMap(): ?Map
+    {
+        return $this->map;
+    }
+
+    public function setMap(?Map $map): self
+    {
+        $this->map = $map;
+
+        return $this;
+    }
+}
